28f80528ae0dfb90b42f3b31cac820b748a0dd0c
krishna <krishnaitdb@gmail.com>
2024-10-13 19:45:32 -0400

    5th commit

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -29,22 +29,38 @@ public_users.get('/author/:author',function (req, res) {
   const authorName = req.params.author;
   let filtered_books = (Object.values(books)).filter((book) => book.author.toLowerCase() === authorName.toLowerCase());
   if (filtered_books.length > 0) {
-    res.send(filtered_books)
+    res.send(filtered_books);
   } else {
-    res.send('No books found for this author ' + authorName)
+    res.send('No books found for this author ' + authorName);
   }
 });
 
 // Get all books based on title
 public_users.get('/title/:title',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const titleName = req.params.title;
+  let filtered_books = (Object.values(books)).filter((book) => book.title.toLowerCase() === titleName.toLowerCase());
+  if (filtered_books.length > 0) {
+    res.send(filtered_books);
+  } else {
+    res.send('No book(s) found by this title ' + titleName);
+  }
 });
 
 //  Get book review
 public_users.get('/review/:isbn',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const isbn = req.params.isbn;
+  if (Object.values(books)[isbn]) {
+    if (Object.values(books)[isbn].reviews.length > 0) {
+        res.send(Object.values(books)[isbn].reviews);
+    } else {
+        res.send('No reviews found for this book ISBN');
+    }
+  } else {
+    res.send('No book found by this book ISBN ' + req.params.isbn);
+  }
+  
 });
 
 module.exports.general = public_users;
