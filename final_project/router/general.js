16688a12b10f496d0250c9704b066b95226032b3
krishna <krishnaitdb@gmail.com>
2024-10-13 21:03:38 -0400

    6th commit

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -4,10 +4,38 @@ let isValid = require("./auth_users.js").isValid;
 let users = require("./auth_users.js").users;
 const public_users = express.Router();
 
+// Check if a user with the given username already exists
+const doesExist = (username) => {
+    // Filter the users array for any user with the same username
+    let userswithsamename = users.filter((user) => {
+        return user.username === username;
+    });
+    // Return true if any user with the same username is found, otherwise false
+    if (userswithsamename.length > 0) {
+        return true;
+    } else {
+        return false;
+    }
+}
 
 public_users.post("/register", (req,res) => {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const username = req.body.username;
+  const password = req.body.password;
+
+  // Check if both username and password are provided
+  if (username && password) {
+      // Check if the user does not already exist
+      if (!doesExist(username)) {
+          // Add the new user to the users array
+          users.push({"username": username, "password": password});
+          return res.status(200).json({message: "User successfully registered. Now you can login"});
+      } else {
+          return res.status(404).json({message: "User already exists!"});
+      }
+  }
+  // Return error if username or password is missing
+  return res.status(404).json({message: "Unable to register user."});
 });
 
 // Get the book list available in the shop
@@ -52,10 +80,10 @@ public_users.get('/review/:isbn',function (req, res) {
   //Write your code here
   const isbn = req.params.isbn;
   if (Object.values(books)[isbn]) {
-    if (Object.values(books)[isbn].reviews.length > 0) {
-        res.send(Object.values(books)[isbn].reviews);
+    if (Object.values(Object.values(books)[isbn]['reviews']).length > 0) {
+        res.send(Object.values(Object.values(books)[isbn]['reviews']));
     } else {
-        res.send('No reviews found for this book ISBN');
+        res.send('No reviews found for this book titled ' + '\'' + Object.values(books)[isbn]['title'] + '\'');
     }
   } else {
     res.send('No book found by this book ISBN ' + req.params.isbn);
